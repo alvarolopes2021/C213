40888d2ecf86d6b433404e0b76404e9625b34d3d
alvaro lopes <alvarolopes2020@protonmail.com>
2023-10-10 20:40:18 -0300

    feat: grafico metodo integral do erro

--- controlpidexemplo.py
+++ controlpidexemplo.py
@@ -9,9 +9,14 @@ Theta = 3.045 # atraso de propagação
 #parâmetros do controlador kp+kp/(Ti*s)+kp*Td*s
 
 #kp = 0.6*14.865 / 3.489 * 3.045 = 0,8395
-kp=0.8395
-Ti=14.865 # == tau
-Td= 1.5225 # 0.5 * theta
+kp_chr=0.8395 
+Ti_chr=14.865 # == tau
+Td_chr= 1.5225 # 0.5 * theta
+
+# por integral do erro
+kp = 0.7079
+Ti = 13.4850
+Td = 0.1651
 
 print(kp)
 print(Ti)
@@ -25,9 +30,38 @@ n_pade = 20
 H_pade = cnt.tf( num_pade , den_pade )
 Hs = cnt.series (H , H_pade)
 
+plt.xlabel ( ' t [ s ] ')
+plt.ylabel('Amplitude')
+plt.title('Controle PID - CHR 1')
 
 
-# Controlador proporcional
+
+# Controlador proporcional -> CHR 1
+numkp = np. array ([kp_chr])
+denkp = np. array ([1])
+#integral
+numki = np. array ([kp_chr])
+denki = np. array ([Ti_chr,0])
+#derivativo
+numkd = np. array ([kp_chr*Td_chr,0])
+denkd = np. array ([1])
+#Construindo o controlador PID
+Hkp = cnt.tf(numkp , denkp)
+Hki=cnt.tf(numki , denki)
+Hkd=cnt.tf(numkd , denkd)
+Hctrl1 = cnt.parallel (Hkp , Hki)
+Hctrl = cnt.parallel (Hctrl1 , Hkd)
+Hdel = cnt.series (Hs , Hctrl)
+#Fazendo a realimentação
+Hcl = cnt.feedback(Hdel, 1)
+
+t = np . linspace (0 , 100 , 100)
+(t , y ) = cnt.step_response (16* Hcl, t )
+plt.plot (t , y, color="yellow")
+
+
+
+# Controlador proporcional -> INTEGRAL DO ERRO
 numkp = np. array ([kp])
 denkp = np. array ([1])
 #integral
@@ -46,17 +80,12 @@ Hdel = cnt.series (Hs , Hctrl)
 #Fazendo a realimentação
 Hcl = cnt.feedback(Hdel, 1)
 
-
 t = np . linspace (0 , 100 , 100)
 (t , y ) = cnt.step_response (16* Hcl, t )
 plt.plot (t , y )
-plt.xlabel ( ' t [ s ] ')
-plt.ylabel('Amplitude')
-plt.title('Controle PID - CHR 1')
 
 
-
-#escrevendo a função de transferência da planta
+###
 num = np. array ([k])
 den = np. array ([tau , 1])
 H = cnt.tf(num , den)
